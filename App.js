44cd05a07e8b08b0243eff2b4d1faf522a8a1d83
sukanya1509 <namsukanya1509@gmail.com>
2020-09-17 04:39:13 +0700

    commit1

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/Weather'
+
+export default function App() {
+
+  return (
+    <View>
+       <Weather zipCode="83120" /> 
+    </View>
+  );
+}
